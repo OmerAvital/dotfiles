a121fa2dd1cfca5ce7e308ce86cbecfe3e8b4f12
omer8374 <76176305+omer8374@users.noreply.github.com>
2021-11-23 00:24:20 -0800

    Update actually fixed this time

--- utils/_update.zsh
+++ utils/_update.zsh
@@ -7,10 +7,13 @@ update_dotfiles() {
     rm "$update_txt_file"
   fi
   local dotfiles_update submodules_update
-  dotfiles_update=$(git pull origin HEAD)
+
+  # Fetch dry run first because pull always produces output
+  dotfiles_update=$(git fetch --dry-run)
   submodules_update=$(cd "$DOTFILES" && git submodule update --init --recursive --remote)
 
   if [[ -n "$dotfiles_update" ]]; then
+    dotfiles_update=$(git pull origin HEAD)
     print "${bg_bold[blue]} Dotfiles updated\! ${reset_color}"
     print "$dotfiles_update\n"
   fi
